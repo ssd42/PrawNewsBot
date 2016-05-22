9debc5733d04309240cf76cbfbefb60bdd49aa1c
Steven Dias <stevendias11@gmail.com>
2016-05-22 12:04:54 -0400

    removed data

--- NewsBot/obot.py
+++ NewsBot/obot.py
@@ -1,15 +1,15 @@
 import praw
 
-app_ua = 'lern praw'
-app_id = 'u5n0FAfmNA3PaA'
-app_secret = 'SrQsLFWVh09TPtbw9gmTHHUhDqw'
-app_uri = 'https://127.0.0.1:65010/authorize_callback'
+app_ua = ''
+app_id = ''
+app_secret = ''
+app_uri = ''
 
 app_scopes = 'account creddits edit flair history identity livemanage modconfig modcontributors modflair modlog modothers modposts modself modwiki mysubreddits privatemessages read report save submit subscribe vote wikiedit wikiread'
-app_account_code = 'Xe32y13roppezbipKENyi9e_o6U'
+app_account_code = ''
 
 
-app_refresh = '42769845-t-4lAOEzKIsKmaKvBuyQfYvaipA'
+app_refresh = ''
 
 # login into my reddit credentials for me
 def login():
